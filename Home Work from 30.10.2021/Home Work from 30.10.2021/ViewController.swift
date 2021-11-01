9ef0368af23b1440b46eb797702cdf6562ca6f8d
Михаил <mihailpachkovskiy@gmail.com>
2021-11-01 13:29:04 +0300

    Initial commit

---
+++ Home Work from 30.10.2021/Home Work from 30.10.2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work from 30.10.2021
+//
+//  Created by Михаил on 1.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
