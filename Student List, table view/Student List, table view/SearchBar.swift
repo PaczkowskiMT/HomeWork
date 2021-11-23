df7b2e6fe6dd0cc3ffa38f40786d5ed7be3e5e84
Михаил <mihailpachkovskiy@gmail.com>
2021-11-23 16:12:09 +0300

    Create new file "SearchBar.swift"

---
+++ Student List, table view/Student List, table view/SearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBar.swift
+//  Student List, table view
+//
+//  Created by Михаил on 23.11.21.
+//
+
+import UIKit
+
+class SearchBar: UISearchBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
