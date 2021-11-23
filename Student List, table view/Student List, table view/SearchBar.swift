1b521606f7a6101644561f7447314cdcb8f12fa9
Михаил <mihailpachkovskiy@gmail.com>
2021-11-23 16:49:16 +0300

    app still does not start

--- Student List, table view/Student List, table view/SearchBar.swift
+++ Student List, table view/Student List, table view/SearchBar.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class SearchBar: UISearchBar {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    extension MasterViewController: UISearchResultsUpdating {
+      func updateSearchResults(for searchController: UISearchController) {
+      }
     }
-    */
 
+    searchController.searchResultsUpdater = self
+    searchController.obscuresBackgroundDuringPresentation = false
+    searchController.searchBar.placeholder = "Search Student"
+    navigationItem.searchController = searchController
+    definesPresentationContext = true
+    
+    var filteredData: StudentsViewController.data = []
+    
+    var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String,
+                                    category: Data? = nil) {
+      filteredData = data.filter { (data: Data) -> Bool in
+        return data.name.lowercased().contains(searchText.lowercased())
+      }
+      
+      tableView.reloadData()
+    }
+
+    let searchBar = searchController.searchBar
+    filterContentForSearchText(searchBar.text!)
 }
