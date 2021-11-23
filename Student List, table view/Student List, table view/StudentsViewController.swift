a91f89a7257d861cec35cfb2329bc49d634afd19
Михаил <mihailpachkovskiy@gmail.com>
2021-11-23 14:09:34 +0300

    Start created search bar

--- Student List, table view/Student List, table view/StudentsViewController.swift
+++ Student List, table view/Student List, table view/StudentsViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class StudentsViewController: UIViewController {
+class StudentsViewController: UITableViewController {
 
-    @IBOutlet weak var tableView: UITableView!
-    let cellIDMale = "cellID"
-    let cellIDFemale = "cellIDFemale"
-    var dataSource = ["Aртимович Игорь Владимирович",
-                          "Богданович Дмитрий Александрович",
+    let data = [["Aртимович Игорь Владимирович",
+                 "Богданович Дмитрий Александрович",
                           "Гришин Павел Андреевич",
                           "Ефименко Анастасия Владимировна",
                           "Куклицкий Максим Сергеевич",
@@ -25,45 +22,37 @@ class StudentsViewController: UIViewController {
                           "Савков Александр Геннадьевич",
                           "Симонов Владислав Дмитриевич",
                           "Сысов Валерий Александрович",
-        ]
-    var femaleDataSource = ["Букаренко Арина Олеговна",
+                ],
+   ["Букаренко Арина Олеговна",
                             "Пернацкая Алеся Юрьевна",
                             "Сандова Галина Александровна",
                             "Елисеева Марина Михайловна",
-    ]
+   ]]
+    let sex: Array<String> = ["Male", "Female"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sex.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data [section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sex[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt section: Int) -> UITableViewCell? {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: data[[String?]])
+        cell.textLabel?.text = data
+        return cell
+    }
+
 
 }
-extension StudentsViewController: UITableViewDelegate, UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return dataSource.count
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return "Male"
-    }
-    private func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: cellIDMale)
-        if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: cellIDMale)
-        }
-        cell?.textLabel!.text = dataSource[indexPath.row]
-        return cell!
-    }
-    }
-extension StudentsViewController: UITableViewDelegate, UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {return femaleDataSource.count
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return "Female"
-    }
-    private func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: cellIDFemale)
-        if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: cellIDFemale)
-        }
-        cell?.textLabel!.text = dataSource[indexPath.row]
-        return cell!
-    }
-    }
+
+
